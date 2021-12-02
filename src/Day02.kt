0a48c688962b05ccd157992d5ccfae220f9b18a4
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-12-02 16:20:07 -0500

    Advent of Code - Day 2

---
+++ src/Day02.kt
@@ -0,0 +1,48 @@
+private const val FORWARD = "forward"
+private const val DOWN = "down"
+private const val UP = "up"
+
+fun main() {
+    val testInput = getPairs(readInput("Day02_test"))
+    check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
+
+    val input = getPairs(readInput("Day02"))
+    println(part1(input))
+    println(part2(input))
+}
+
+private fun part1(input: List<Pair<String, Int>>): Int {
+    var horizontal = 0
+    var depth = 0
+
+    input.forEach { (direction, num) ->
+        when (direction) {
+            FORWARD -> horizontal += num
+            DOWN -> depth += num
+            UP -> depth -= num
+        }
+    }
+    return horizontal * depth
+}
+
+private fun part2(input: List<Pair<String, Int>>): Int {
+    var horizontal = 0
+    var depth = 0
+    var aim = 0
+
+    input.forEach { (direction, num) ->
+        when (direction) {
+            FORWARD -> {
+                horizontal += num
+                depth += num * aim
+            }
+            DOWN -> aim += num
+            UP -> aim -= num
+        }
+    }
+    return horizontal * depth
+}
+
+private fun getPairs(input: List<String>) =
+    input.map { string -> string.split(" ").let { it[0] to it[1].toInt() } }
