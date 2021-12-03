bf5eb1fdb2d217b50062abbcc8d4f2fd1f98ef57
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-12-02 21:06:04 -0500

    Create Template - Update
    • I found https://curlconverter.com/# which creates a Java file ( which I then converted to Kotlin ) that I can use to retrieve and save the file. Updated main() to use the file, sending it the day to fetch and the file to save it to ( for obvious reasons, I will not be uploading the FetchInput.kt file ).

--- src/CreateTemplate.kt
+++ src/CreateTemplate.kt
@@ -8,8 +8,7 @@ private val TEMPLATE = getFile("$FOLDER/template.txt")
 private val KT_FILE = getFile("Day$DAY.kt")
 
 fun main() {
-    INPUT.createNewFile()
     TEST.createNewFile()
+    if (!INPUT.exists()) FetchInput.toFile(DAY.toInt(), INPUT)
     if (!KT_FILE.exists()) KT_FILE.writeText(String.format(TEMPLATE.readText(), DAY, DAY))
 }
-
