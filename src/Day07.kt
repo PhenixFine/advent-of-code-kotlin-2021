5d236e4947e7327872aa32cc7e6f788b3d1572cf
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-12-08 12:05:28 -0500

    Advent of Code - Day 7
    • I realized that in getCrabs function it would have been better to leave the data retrieved from the file as is, instead of putting a count of each number into an array. I adjusted the other functions accordingly, and moved the math of function sumOfNumber to where it was called. Also, I removed the min and max check in getSearchIndexes, as I realized it wasn't necessary ( I had originally put it up because I was getting index out of bounds errors, but that was only because I didn't have the correct while loop condition at the time ).
    • I found out that the challenge can be solved much more simply with math, but figured I'd just keep how I went about it up, as there's already a bunch of solutions that solve it mathematically.

--- src/Day07.kt
+++ src/Day07.kt
@@ -15,16 +15,11 @@ private fun part1(input: List<Int>) = calculate(input)
 
 private fun part2(input: List<Int>) = calculate(input, true)
 
-private fun getCrabs(input: List<String>): List<Int> {
-    val numbers = input.first().split(",").map { it.toInt() }
-    val crabs = IntArray((numbers.maxOrNull() ?: 0) + 1)
-
-    numbers.forEach { crabs[it]++ }
-    return crabs.toList()
-}
+private fun getCrabs(input: List<String>) = input.first().split(",").map { it.toInt() }
 
 private fun calculate(input: List<Int>, isSumOf: Boolean = false): Int {
-    var checkSet = setOf(0, input.lastIndex / 2, input.lastIndex)
+    val max = input.maxOrNull() ?: 0
+    var checkSet = setOf(0, max / 2, max)
     var lastSet = setOf<Int>()
     var lowestFuel = Pair(0, Int.MAX_VALUE)
 
@@ -33,7 +28,7 @@ private fun calculate(input: List<Int>, isSumOf: Boolean = false): Int {
         if (fuel.first().second < lowestFuel.second) lowestFuel = fuel.first()
         fuel = (if (fuel.size > 2) fuel.subList(0, 2) else fuel).sortedBy { it.first }
         lastSet = checkSet
-        checkSet = if (fuel.size == 1) setOf(fuel.first().first) else getSearchIndexes(fuel, input.lastIndex)
+        checkSet = if (fuel.size == 1) setOf(fuel.first().first) else getSearchIndexes(fuel)
     }
     return lowestFuel.second
 }
@@ -41,20 +36,16 @@ private fun calculate(input: List<Int>, isSumOf: Boolean = false): Int {
 private fun calculateFuel(input: List<Int>, moveTo: Int, isSumOf: Boolean): Int {
     var fuel = 0
 
-    for ((index, count) in input.withIndex()) {
-        if (count > 0 && index != moveTo) {
-            val num = abs(index - moveTo)
-            fuel += (if (isSumOf) sumOfNumber(num) else num) * count
-        }
+    for (number in input) {
+        val num = abs(number - moveTo)
+        fuel += if (isSumOf) num * (1 + num) / 2 else num
     }
     return fuel
 }
 
-private fun getSearchIndexes(input: List<Pair<Int, Int>>, max: Int): Set<Int> {
-    val first = (input[0].first + 1).coerceAtMost(max)
-    val last = (input[1].first - 1).coerceAtLeast(0)
+private fun getSearchIndexes(input: List<Pair<Int, Int>>): Set<Int> {
+    val first = input[0].first + 1
+    val last = input[1].first - 1
 
     return setOf(first, first + (last - first) / 2, last)
 }
-
-private fun sumOfNumber(num: Int) = num * (1 + num) / 2
