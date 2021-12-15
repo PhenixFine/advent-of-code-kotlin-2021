8c4fa40dc4fc245f6e5748410db3fa4ad462d665
Danny <64429863+PhenixFine@users.noreply.github.com>
2021-12-14 20:11:35 -0500

    Advent of Code - Utils Coordinate.kt
    • updated to lesson the hassle of using lists with it; and added an extension function for adding x, y coordinates to it.

--- src/utils/Coordinate.kt
+++ src/utils/Coordinate.kt
@@ -1,6 +1,18 @@
 package utils
 
-data class Coordinate(val row: Int, val column: Int) {
+data class Coordinate(val rowY: Int, val columnX: Int) {
 
-    operator fun plus(coordinate: Coordinate) = Coordinate(row + coordinate.row, column + coordinate.column)
+    operator fun plus(coordinate: Coordinate) = Coordinate(rowY + coordinate.rowY, columnX + coordinate.columnX)
+}
+
+fun String.xyToCoordinate(divider: String = ",") = split(divider).let {
+    return@let Coordinate(it.last().toInt(), it.first().toInt())
+}
+
+fun <T> List<List<T>>.getOrNull(coordinate: Coordinate) = this.getOrNull(coordinate.rowY)?.getOrNull(coordinate.columnX)
+
+operator fun <T> List<List<T>>.get(coordinate: Coordinate) = this[coordinate.rowY][coordinate.columnX]
+
+operator fun <T> MutableList<MutableList<T>>.set(coordinate: Coordinate, value: T) {
+    this[coordinate.rowY][coordinate.columnX] = value
 }
